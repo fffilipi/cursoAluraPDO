ad1d5dd72a227705fd0aeeea150fa9f561892841
Filipi Nata <filipi_f@hotmail.com>
2023-05-19 22:09:19 -0300

    aluraPdoPhph - insert and list students

--- conexao.php
+++ conexao.php
@@ -1,9 +1,8 @@
 <?php
 
-$caminhoBanco = __DIR__ . 'banco.sqlite';
-
-$pdo = new PDO('sqlite:' . $caminhoBanco);
-
+$databasePath = __DIR__ . 'banco.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
 //'mysql:host=endereco_do_servidor;dbname=nome_do_banco'
-
 echo 'conectado';
+
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
