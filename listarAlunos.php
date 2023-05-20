a73c5e29d88263429ccdbc733ff8f28d21796275
Filipi Nata <filipi_f@hotmail.com>
2023-05-19 22:16:14 -0300

    aluraPdoPhp - alternative PDO

--- listarAlunos.php
+++ listarAlunos.php
@@ -1,10 +1,21 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$statement = $pdo->query('SELECT * FROM students');
+$statement = $pdo->query('SELECT * FROM students;');
+$studentDataList = $statement->fetchAll(fetch_style: PDO::FETCH_ASSOC);
+$studentList = [];
 
-var_dump($statement->fetchAll());
+foreach ($studentDataList as $studentData) {
+    $studentList[] = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+}
+
+var_dump($studentList);
