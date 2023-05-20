104074a76fd0dc419b533e0ea2cab6da1984f316
Filipi Nata <filipi_f@hotmail.com>
2023-05-19 22:40:19 -0300

    aluraPdoPhp - security sql

--- inserirAluno.php
+++ inserirAluno.php
@@ -9,6 +9,12 @@ $pdo = new PDO('sqlite:' . $databasePath);
 
 $student = new Student(null, "Vinicius Dias", new \DateTimeImmutable(1997-10-15));
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES ('{$student->name()}', '{$student->birthDate()->format('Y-m-d')}')";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
 
-var_dump($pdo->exec($sqlInsert));
+$statement = $pdo->prepare($sqlInsert);
+$statement->bindValue(1, $student->name());
+$statement->bindValue(2, $student->birthDate()->format(format:'Y-m-d'));
+
+if ($statement->execute()) {
+    echo "Aluno incluído";
+}
